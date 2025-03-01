e4ca2066b061a64fbefaa5f0bb989ca6f59a93be
kouta <takeuchi180121@gmail.com>
2025-03-01 18:48:20 +0900

    各RouteをlazyImport

--- src/client/components/application/Routes/Routes.tsx
+++ src/client/components/application/Routes/Routes.tsx
@@ -1,11 +1,11 @@
-import type { FC } from 'react';
+import {type FC, lazy } from 'react';
 import * as Router from 'react-router-dom';
 
-import { NotFound } from '../../../pages/NotFound';
-import { Order } from '../../../pages/Order';
-import { OrderComplete } from '../../../pages/OrderComplete';
-import { ProductDetail } from '../../../pages/ProductDetail';
-import { Top } from '../../../pages/Top';
+const Top = lazy(() => import('../../../pages/Top').then((mod) => ({ default: mod.Top })));
+const ProductDetail = lazy(() => import('../../../pages/ProductDetail').then((mod) => ({ default: mod.ProductDetail })));
+const Order = lazy(() => import('../../../pages/Order').then((mod) => ({ default: mod.Order })));
+const OrderComplete = lazy(() => import('../../../pages/OrderComplete').then((mod) => ({ default: mod.OrderComplete })));
+const NotFound = lazy(() => import('../../../pages/NotFound').then((mod) => ({ default: mod.NotFound })));
 
 import { useScrollToTop } from './hooks';
 
